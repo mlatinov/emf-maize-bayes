5e7d55693ac406e93af62685a7d4a034e8faf2eb
mlatinov <latinovmetodi@gmail.com>
2026-06-01 01:19:50 +0300

    function to clean the table 1 from maize height data

--- R/clean_data_.R
+++ R/clean_data_.R
@@ -12,8 +12,16 @@ clean_meize_raw <- function(maize_heigh_raw){
     ) %>%
     # Fix the Types
     mutate(
-      day = as.integer(day),
+      day = as.integer(factor(day, levels = c(7, 14, 20, 27))),
       pot = as.integer(pot),
-      plant_id = as.integer(plant_id) 
+      plant_id  = as.integer(plant_id),
+      treatment = as.integer(factor(treatment,levels = c("Control","Sham","EMF")))
+    ) %>%
+    # Remove Plants where Height = 0
+    filter(plant_height > 0) %>%
+    # Build the pot index 
+    arrange(treatment, pot) %>%
+    mutate(
+      pot_id = as.integer(factor(paste(treatment, pot)))
     )
 }
