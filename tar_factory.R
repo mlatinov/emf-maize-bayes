cc9d09b344084951242b9cca93c3122501b42da4
mlatinov <latinovmetodi@gmail.com>
2026-06-22 21:56:25 +0300

    refactior the maize height pipeline

---
+++ tar_factory.R
@@ -0,0 +1,48 @@
+#### Maize Height Pipeline Function ####
+maize_height_pipeline <- function(){
+  list(
+  # Load the Table 1 
+  tar_target(
+    name = maize_heigh_raw,
+    command = readxl::read_excel("data/Table S1 - 2026 Paunov et al. - 868 MHz EMF Maize - Plant Height.xlsx",sheet = 1)
+  ),
+  # Clean Table 1
+  tar_target(
+    name = model_data_maize_heigh,
+    command = clean_meize_raw(maize_heigh_raw)
+  ),
+  ## Hierarchical cell-means, pots pooled within treatment ##
+  tar_target(
+    name = m5_recovery_data,
+    command = maize_h_m5_dgp(n = 700)
+  ),
+  tar_target(
+    name = m5_recovery_check,
+    command = m5_stan_model(m5_recovery_data$data, prior_only = 0)
+  ),
+  tar_target(
+    name = m5_maize_model,
+    command = m5_stan_model(model_data_maize_heigh, prior_only = 0)
+  ),
+  tar_target(
+    name = m5_maize_model_pp,
+    command = m5_stan_model(model_data_maize_heigh, prior_only = 1)
+  ),
+  ## Hierarchical Gompertz, per-pot curves pooled within treatment ##
+  tar_target(
+    name = m6_recovery_data,
+    command = maize_h_m6_dgp(n = 700)
+  ),
+  tar_target(
+    name = m6_recovery_check,
+    command = m6_stan_model(m6_recovery_data$data, prior_only = 0)
+  ),
+  tar_target(
+    name = m6_maize_model,
+    command = m6_stan_model(model_data_maize_heigh, prior_only = 0)
+  ),
+  tar_target(
+    name = m6_maize_model_pp,
+    command = m6_stan_model(model_data_maize_heigh, prior_only = 1)
+  ))
+}
