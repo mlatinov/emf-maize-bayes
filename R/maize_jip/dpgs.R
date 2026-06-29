32dabb2e7b6556313cfb24ba7ea34c9ac7966244
mlatinov <latinovmetodi@gmail.com>
2026-06-29 22:16:16 +0300

    add combined simulation for the initial estimands correlation not included

--- R/maize_jip/dpgs.R
+++ R/maize_jip/dpgs.R
@@ -1,5 +1,6 @@
 
 #### Function to for Simulating Data Generative Processes to test the model ####
+set.seed(42)
 
 #### Cell- Means model Without Pooling ####
 dgp_gammaRC_jip_1 <- function(
@@ -88,4 +89,150 @@ dgp_gammaRC_jip_2 <- function(
       u_pot    = u_pot
     )
   ))
+}
+
+#### Cell Means Combined Across all estimands ####
+dgp_combined_jip <- function(
+  n = 188,
+  prob_day = c(64, 64, 60) / 188,
+  prob_pot = c(12,12,12,12,12,12, 12,12,12,12, 12,12,12,12,8,12) / 188
+){
+  # Design 
+  day           <- sample(1:3, size = n, replace = TRUE, prob = prob_day)
+  pot_treatment <- c(rep(1, 6), rep(2, 4), rep(3, 6))  
+  pot_idx       <- sample(1:16, size = n, replace = TRUE, prob = prob_pot)
+  treatment     <- pot_treatment[pot_idx]
+
+  ## GammaRC Model ## ====================================================
+  # Parameters 
+  g_kappa_d <- rexp(3, rate = 0.01)        # concentration per day
+  g_tau_t   <- rexp(3, rate = 5)           # pot-level SD per treatment 
+  g_z_p     <- rnorm(16, mean = 0, sd = 1) # standardized per-pot deviate
+  g_eta     <- matrix(rnorm(9, mean = -1, sd = 0.3), nrow = 3, ncol = 3)  
+
+  # per-pot offset
+  g_u_pot <- g_z_p * g_tau_t[pot_treatment]    # length 16
+
+  # per-obs 
+  g_eta_i   <- g_eta[cbind(treatment, day)]  # scalar per obs
+  g_u_i     <- g_u_pot[pot_idx]              # obs pot offset
+  g_mu_i    <- plogis(g_eta_i + g_u_i)         # probability scale
+  g_kappa_i <- g_kappa_d[day]      
+  
+  # Draw from Beta Distribution 
+  gammaRC <- rbeta(
+    n = n,
+    shape1 = g_mu_i * g_kappa_i,
+    shape2 = (1 - g_mu_i) * g_kappa_i
+  )
+
+  ## Phi Model ## ========================================================
+  # Parameters
+  phi_eta_dt  <- matrix(rnorm(n = 9, mean = 0.8, sd = 0.3),nrow = 3, ncol = 3)
+  phi_kappa_d <- rexp(n = 3, rate = 0.001)
+  phi_tau_t   <- rexp(n = 3, rate = 3)
+  phi_zp      <- rnorm(n = 16, mean = 0, sd = 1)
+
+  # Per pot offect 
+  phi_up <- phi_tau_t[pot_treatment] * phi_zp
+
+  # Per observation 
+  phi_eta_dti   <- phi_eta_dt[cbind(treatment, day)] 
+  phi_kappa_d_i <- phi_kappa_d[day] 
+  phi_up_i      <- phi_up[pot_idx] 
+  phi_mu_i      <- plogis(phi_eta_dti + phi_up_i)
+
+  # Draw from beta distribution 
+  phi_po <- rbeta(
+    n = n,
+    shape1 = phi_mu_i * phi_kappa_d_i,
+    shape2 = (1 - phi_mu_i) * phi_kappa_d_i
+  )
+
+  ## Psi Model ## ===========================================================
+  # Paramters 
+  psi_eta_dt  <- matrix(rnorm(n = 9, mean = 0.7, sd = 0.3), nrow = 3, ncol = 3)
+  psi_kappa_d <- rexp(n = 3, rate = 0.01) 
+  psi_tau_t   <- rexp(n = 3, rate = 3)
+  psi_zp      <- rnorm(n = 16, mean = 0, sd = 1)
+
+  # Per pot offcet 
+  psi_up <- psi_tau_t[pot_treatment] * psi_zp
+
+  # Per obs 
+  psi_eta_td_i  <- psi_eta_dt[cbind(treatment, day)]
+  psi_up_i      <- psi_up[pot_idx]
+  psi_kappa_d_i <- psi_kappa_d[day] 
+  psi_mu_i      <- plogis(psi_eta_td_i + psi_up_i)
+
+  # Draw from beta Distribution 
+  psi_Eo <- rbeta(
+    n = n,
+    shape1 = psi_mu_i * psi_kappa_d_i,
+    shape2 = (1 - psi_mu_i) * psi_kappa_d_i
+  )
+
+  ## Ro Model ## ============================================================
+  # Paramters 
+  ro_eta_dt   <- matrix(rnorm(n = 9, mean = 0.1, sd = 0.7),nrow = 3, ncol = 3)
+  ro_kappa_d  <- rexp(n = 3, rate = 0.01) 
+  ro_tau_t    <- rexp(n = 3, rate = 2)
+  ro_zp       <- rnorm(n = 16, mean = 0, sd = 1)
+  
+  # Pot Offcet 
+  ro_up <- ro_tau_t[pot_treatment] * ro_zp
+
+  # Per obs
+  ro_eta_dt_i  <- ro_eta_dt[cbind(treatment, day)]
+  ro_kappa_d_i <- ro_kappa_d[day]
+  ro_up_i      <- ro_up[pot_idx]
+  ro_mu_i      <- plogis(ro_eta_dt_i + ro_up_i)
+
+  # Draw from Beta Distribution 
+  delta_Ro <- rbeta(
+    n = n,
+    shape1 = ro_mu_i * ro_kappa_d_i,
+    shape2 = (1 - ro_mu_i) * ro_kappa_d_i
+  )
+
+  ## Combine in one dataset ##
+  sim_data <- data.frame(
+    day_idx   = day,
+    treatment = treatment,
+    pot_idx   = pot_idx,
+    delta_Ro = delta_Ro,
+    psi_Eo   = psi_Eo,
+    phi_po   = phi_po,
+    gammaRC  = gammaRC
+  )
+
+  # Combine the paramters in a list 
+  paramters <- list(
+    gammaRC = list(
+      kappa_d = g_kappa_d,   
+      tau     = g_tau_t,          
+      zp      = g_z_p,
+      eta     = g_eta  
+    ),
+    phi_Po = list(
+      eta   = phi_eta_dt,
+      kappa = phi_kappa_d,
+      tau   = phi_tau_t,
+      zp    = phi_zp
+    ),
+    psi_Eo = list(
+      eta   = psi_eta_dt,
+      kappa = psi_kappa_d,
+      tau   = psi_tau_t,
+      zp    = psi_zp
+    ),
+    delta_Ro = list(
+      eta   = ro_eta_dt,
+      kappa = ro_kappa_d,
+      tau   = ro_tau_t,
+      zp    = ro_zp
+    )
+  )
+  # Return Simulated Data and Parameters 
+  return(list(data = sim_data, parameters = paramters))
 }
