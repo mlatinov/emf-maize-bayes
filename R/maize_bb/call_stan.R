12cb579e0ee814e2280a891c3aeb38957b4f8b08
mlatinov <latinovmetodi@gmail.com>
2026-09-03 21:20:09 +0300

    change the output to include the data passed into stan required for effect curves

--- R/maize_bb/call_stan.R
+++ R/maize_bb/call_stan.R
@@ -1,18 +1,40 @@
+#### Function to Construct effect size curves 
+get_effect_size_grid <- function(var, g){
+  var_log_range <- range(log(var))
+  var_grid      <- seq(
+    (var_log_range[1] - mean(log(var))) / sd(log(var)),
+    (var_log_range[2] - mean(log(var))) / sd(log(var)),
+  length.out = g
+  )
+  return(var_grid)
+}
+
 #### Function to Call Stan model M1 Maize BB ####
 bb_model <- function(
   data_bb, 
   prior_only   = 0, 
   stan_file    = "Stan/maize_biochem_biomass/M1_maize_bb.stan", 
   iter         = 2000, 
-  adapt_delta  = 0.95
+  adapt_delta  = 0.95,
+  grid_points  = 50
 ){
 
   ## Prepare the data for Stan 
   data             <- data_bb$data
   pot_treatment_id <- data_bb$additional$pot_treatment_id
 
+  ## Construct effect size curves
+  sod_z_grid  <- get_effect_size_grid(data_bb$data$sod_dw,   g = grid_points)
+  cat_z_grid  <- get_effect_size_grid(data_bb$data$cat_dw,   g = grid_points)
+  h2o2_z_grid <- get_effect_size_grid(data_bb$data$h2_o2_dw, g = grid_points)
+  
+  ## Combine everything in Stan data list to pass to cmdstanr
   stan_data <- list(
     N = nrow(data),
+    G = grid_points,
+    sod_z_grid  = sod_z_grid,
+    h202_z_grid = cat_z_grid,
+    cat_z_grid  = h2o2_z_grid,
     prior_only = prior_only,
     pot_treatment_id = pot_treatment_id,
     pot_id           = data$pot_id,
@@ -40,5 +62,5 @@ bb_model <- function(
     seed          = 42,
     adapt_delta   = adapt_delta
   )
-  return(sample)
+  return(list(sample = sample, stan_data = stan_data))
 }
